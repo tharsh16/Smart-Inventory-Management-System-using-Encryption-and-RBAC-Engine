80fe46f0145bac553a515f5930656b48ad830b59
tharsh16 <ptharshanaa@gmail.com>
2026-07-08 22:43:44 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from database import create_tables
+
+create_tables()
+
+print("===================================")
+print(" Smart Inventory Database Created ")
+print("===================================")
+print("Users table created successfully.")
+print("Inventory table created successfully.")
